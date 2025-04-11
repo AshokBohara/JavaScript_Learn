342079f50f403051adbf85280e859d9a28aa1118
Ashok Bohara <boharaashok32@gmail.com>
2025-04-11 19:26:43 +0545

    modified

--- Basic/main.js
+++ Basic/main.js
@@ -10,66 +10,66 @@ let b=false;
 console.log(a&&getName())
 
 //*Template Litrals
-let Firstname="ashok";
-let surname="bohara";
-let prefix="Er";
+let Firstname="Ashok";
+let surname="Bohara";
+let prefix="Engineer";
 // console.log(prefix+ " "+Firstname + " " +surname);
 console.log(`${prefix} ${Firstname} ${surname}`);
 
 //*Ternary Operators
 
-let returnAge =true;
-function getAgeInfo(age)
-{
-    return `This person is ${age} year old`;
-}
-function getCity()
-{
-    return 'i am from Nepal';
-}
+// let returnAge =true;
+// function getAgeInfo(age)
+// {
+//     return `This person is ${age} year old`;
+// }
+// function getCity()
+// {
+//     return 'i am from Nepal';
+// }
 
-//this is case of ifelse condition checking..
+// //this is case of ifelse condition checking..
 
-if (returnAge)
+// if (returnAge)
     
-{
-    console.log(getAgeInfo(21))
-}
-else
-{
-    console.log(getCity())
-}
-But in ternary operator we check like..
+// {
+//     console.log(getAgeInfo(21))
+// }
+// else
+// {
+//     console.log(getCity())
+// }
+// //But in ternary operator we check like..
 
-checking = condition ? statementFortrue : statementForFalse
+// checking = condition ? statementFortrue : statementForFalse
 
-returnAge ? console.log(getAgeInfo(21)) : console.log(getCity())
+// returnAge ? console.log(getAgeInfo(21)) : console.log(getCity())
 
-//* shorthand property Names
+// //* shorthand property Names
 
-const id =1;
-const title='product 1';
-const rating =5;
+// const id =1;
+// const title='product 1';
+// const rating =5;
 
-const product=
-{
-    id,
-    title,
-    rating
-}
-console.log(product);
+// const product=
+// {
+//     id,
+//     title,
+//     rating
+// }
+// console.log(product);
 
 
-//* Object and array destructuring
+// //* Object and array destructuring
 
-const productone=
-{
-    productName :'productone',
-    productDescription : 'product Description'
-}
+// const productone=
+// {
+//     productName :'productone',
+//     productDescription : 'product Description'
+// }
 
-let productName=productone.productName;
-let productDescription=productone.productDescription;
+// let productName=productone.productName;
+// let productDescription=productone.productDescription;
 
-const {}=productone
-console.log(productName,productDescription);
+// const {}=productone
+// console.log(productName,productDescription);
--- Basic/ternaryOperator.js
+++ Basic/ternaryOperator.js
@@ -2,10 +2,10 @@
 let returnhome=true;
  function getHome()
  {
-    return 'my home is located in bajhang'
+    return 'my home is located in bajhang district'
  }
  function gethouseNo()
  {
-    return 'house No. is 221'
+    return 'house No. is 777'
  }
  returnhome? console.log(getHome()):console.log(gethouseNo());
