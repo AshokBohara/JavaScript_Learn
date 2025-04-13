a9182d128ecc6b7f38ab2f1699ea94376aa95ebc
Ashok Bohara <boharaashok32@gmail.com>
2025-04-13 20:40:29 +0545

    modified

--- Basic/main.js
+++ Basic/main.js
@@ -8,8 +8,7 @@ function getName ()
 let a=true;
 let b=false;
 console.log(a&&getName())
-
-//*Template Litrals
+// *Template Litrals
 let Firstname="Ashok";
 let surname="Bohara";
 let prefix="Engineer";
@@ -18,15 +17,15 @@ console.log(`${prefix} ${Firstname} ${surname}`);
 
 //*Ternary Operators
 
-// let returnAge =true;
-// function getAgeInfo(age)
-// {
-//     return `This person is ${age} year old`;
-// }
-// function getCity()
-// {
-//     return 'i am from Nepal';
-// }
+let returnAge =true;
+function getAgeInfo(age)
+{
+    return `This person is ${age} year old`;
+}
+function getCity()
+{
+    return 'i am from Nepal';
+}
 
 // //this is case of ifelse condition checking..
 
@@ -43,33 +42,34 @@ console.log(`${prefix} ${Firstname} ${surname}`);
 
 // checking = condition ? statementFortrue : statementForFalse
 
-// returnAge ? console.log(getAgeInfo(21)) : console.log(getCity())
+returnAge ? console.log(getAgeInfo(21)) : console.log(getCity())
+ //* shorthand property Names
 
-// //* shorthand property Names
+const id =1;
+const title='product 1';
+const rating =5;
+const value=7;
 
-// const id =1;
-// const title='product 1';
-// const rating =5;
-
-// const product=
-// {
-//     id,
-//     title,
-//     rating
-// }
-// console.log(product);
+const product=
+{
+    id,
+    title,
+    rating,
+    value
+}
+console.log(product);
 
 
 // //* Object and array destructuring
 
-// const productone=
-// {
-//     productName :'productone',
-//     productDescription : 'product Description'
-// }
+const productone=
+{
+    productName :'productone',
+    productDescription : 'product Description'
+}
 
-// let productName=productone.productName;
-// let productDescription=productone.productDescription;
+let productName=productone.productName;
+let productDescription=productone.productDescription;
 
-// const {}=productone
-// console.log(productName,productDescription);
+const {}=productone
+console.log(productName,productDescription);
