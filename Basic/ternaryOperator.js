06ee5133d3ba0426161d61ccd062988893538db7
Ashok Bohara <boharaashok32@gmail.com>
2026-04-29 22:14:11 +0545

    ternary operator in js

--- Basic/ternaryOperator.js
+++ Basic/ternaryOperator.js
@@ -1,11 +1,11 @@
 //ternary operator in javascripts
 let returnhome=true;
- function getHome()
- {
-    return 'my home is located in bajhang district'
- }
- function gethouseNo()
- {
-    return 'house No. is 777'
- }
- returnhome? console.log(getHome()):console.log(gethouseNo());
+function getHome()
+{
+   return 'my home is located in bajhang district'
+}
+function gethouseNo()
+{
+   return 'house No. is 777'
+}
+returnhome? console.log(getHome()):console.log(gethouseNo());
